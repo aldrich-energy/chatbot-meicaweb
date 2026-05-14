160096a0ae892b36ff00c2c08c2da1a33b45a22d
Aldrich Energy <energyaldrich@gmail.com>
2026-05-14 12:42:41 +0400

    Update NOTIFICATION_API URL in chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,7 +1,7 @@
 (function () {
   const SHEET_URL = "https://script.google.com/macros/s/AKfycbwLgLdTakPKptg3vqwIQSii__l7Q31VOwBLWQGUdWVtCVsKrZMSLR8b0tBaKVbVh3uNiQ/exec";
   const MAILBITE_KEY = "IeWdPN5hoRR7eoSGHmhJQol0WP18331ZaPsa";
-  const NOTIFICATION_API = "https://aldrich-energy-htx9.vercel.app/api/meica-email-notification"; 
+  const NOTIFICATION_API = "https://aldrich-energy.vercel.app/api/meica-email-notification"; 
   const WEBSITE_NAME = "MEICA EXPO"; // ← change this per website
 
   const css = `
